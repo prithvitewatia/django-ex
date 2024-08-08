8281492180fbecca6f2fdb5eda88b1e37eef6edf
prithvitewatia <prithvisinghtewatia@gmail.com>
2024-08-08 17:33:48 +0530

    Updated max length for welcome

--- welcome/models.py
+++ welcome/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class PageView(models.Model):
-    hostname = models.CharField(max_length=32)
+    hostname = models.CharField(max_length=256)
     timestamp = models.DateTimeField(auto_now_add=True)
